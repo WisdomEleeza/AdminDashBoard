7b720497bb6e08066ae5d6ae985afb3b799ace8f
Wisdom Eleeza <whiy07@gmail.com>
2023-03-30 15:17:19 +0000

    Javascript functionality

---
+++ orders.js
@@ -0,0 +1,50 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36578',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+
+    {
+        productName: 'Ruko Fill Pro Drone',
+        productNumber: '45678',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96451',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22874',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: 'DJI Air 25',
+        productNumber: '81357',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '00735',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 const sideMenu = document.querySelector("aside")
 const menuButton = document.querySelector("#menu-btn")
 const closeButton = document.querySelector("#close-btn")
+const themeToggler = document.querySelector(".theme-toggler")
 
-
-menuFunction = () => {
+//show sideBar
+menuButtonFunction = () => {
     sideMenu.style.display = 'block'
 }
 
+menuButton.addEventListener('click', menuButtonFunction)
+
+//close sideBar
 closeBtnFunction = () => {
     sideMenu.style.display = 'none'
 }
 
-menuButton.addEventListener('click', menuFunction)
-
 closeButton.addEventListener('click', closeBtnFunction)
+
+
+//change Theme
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables')
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active') 
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
+    // themeToggler.querySelector('span').classList.toggle('active')
+})
+
+//fill orders in the table
+Orders.forEach(order => {
+    
+})
